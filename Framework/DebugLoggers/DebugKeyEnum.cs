e80eb2623f5ac506be6cd7505c1d52d7a75ea082
cortexA233 <cortexA233@outlook.com>
2025-02-08 01:39:58 +0800

    update namespace

--- Framework/DebugLoggers/DebugKeyEnum.cs
+++ Framework/DebugLoggers/DebugKeyEnum.cs
@@ -2,31 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static partial class KDebugLogger
+
+namespace KToolkit
 {
-    public static Dictionary<string, bool> debuggerConfig = new Dictionary<string, bool>();
-
-    static KDebugLogger()
+    public static partial class KDebugLogger
     {
-        debuggerConfig["UI"] = true;
-        debuggerConfig["Battle"] = true;
-        debuggerConfig["System"] = true;
-        debuggerConfig["Player"] = true;
-        debuggerConfig["Level"] = true;
-        
-        debuggerConfig["Cortex"] = true;
-        debuggerConfig["Veyo"] = false;
-        debuggerConfig["Shin"] = true;
-    }
+        public static Dictionary<string, bool> debuggerConfig = new Dictionary<string, bool>();
 
-    private static string DebuggerConcatArgs(params object[] args)
-    {
-        string res = "";
-        foreach (var item in args)
+        static KDebugLogger()
         {
-            res += item.ToString();
-            res += "  ";
+            debuggerConfig["UI"] = true;
+            debuggerConfig["Battle"] = true;
+            debuggerConfig["System"] = true;
+            debuggerConfig["Player"] = true;
+            debuggerConfig["Level"] = true;
+        
+            debuggerConfig["Cortex"] = true;
+            debuggerConfig["Veyo"] = false;
+            debuggerConfig["Shin"] = true;
         }
-        return res;
+
+        private static string DebuggerConcatArgs(params object[] args)
+        {
+            string res = "";
+            foreach (var item in args)
+            {
+                res += item.ToString();
+                res += "  ";
+            }
+            return res;
+        }
     }
+
 }
